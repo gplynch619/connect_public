da077463b7091f93edf8a7fc315c0029c80a96a2
Gabe Lynch <fireballs619@gmail.com>
2023-07-14 13:44:08 -0500

    fixed join output

--- source/join_output.py
+++ source/join_output.py
@@ -12,6 +12,8 @@ class CreateSingleDataFile():
                 self.path  = os.path.join(path, f'number_{i}')
             except:
                 self.path  = os.path.join(path, f'N-{self.param.N}')
+        if param.sampling == "recompute":
+            self.path = os.path.join(path, "number_0")
         else:
             self.path  = os.path.join(path, f'N-{self.param.N}')
         

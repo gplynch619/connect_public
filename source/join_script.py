a33b5e16b5a120dd61696ecc4637924745efd64d
Gabe Lynch <fireballs619@gmail.com>
2023-07-18 22:49:13 -0700

    fix to join script

--- source/join_script.py
+++ source/join_script.py
@@ -1,12 +1,12 @@
 import sys
 from default_module import Parameters
 import os
-from tools import join_data_files
-
+from source.join_output import CreateSingleDataFile
 
 CONNECT_PATH  = os.path.realpath(os.path.dirname(__file__))
 param_file    = sys.argv[1]
 param         = Parameters(param_file)
 path = CONNECT_PATH + f'/data/{param.jobname}/'
 
-join_data_files(param)
+CSDF = CreateSingleDataFile(param, CONNECT_PATH)
+CSDF.join()
